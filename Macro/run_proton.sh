c2c9ae5060809027faf9c784f7d33e29c3d5440f
easel7 <blankeasel7@gmail.com>
2026-06-26 19:40:45 +0200

    update

--- Macro/run_proton.sh
+++ Macro/run_proton.sh
@@ -2,6 +2,7 @@
 exe=/Users/xiongzheng/software/Hadr03/build/Hadr03
 Tag=Proton
 
+<<<<<<< Updated upstream
 # ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10GeV.mac
 # ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_100GeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_1TeV.mac
@@ -12,6 +13,12 @@ ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10GeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_100GeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_1TeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10TeV.mac
+=======
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_20GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_50GeV.mac
+# ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_1TeV.mac
+# ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10TeV.mac
+>>>>>>> Stashed changes
 
 git add -A
 git commit -m "Auto update $(date '+%F %T')" || true
