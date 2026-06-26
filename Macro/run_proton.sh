6154238e54fac5781918cb0c7708a91e6b5a22a2
easel7 <blankeasel7@gmail.com>
2026-06-26 18:11:38 +0200

    Auto update 2026-06-26 18:11:38

--- Macro/run_proton.sh
+++ Macro/run_proton.sh
@@ -2,9 +2,17 @@
 exe=/Users/xiongzheng/software/Hadr03/build/Hadr03
 Tag=Proton
 
+# ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10GeV.mac
+# ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_100GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_1TeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10TeV.mac
+
+Tag=Deuteron
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10GeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_100GeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_1TeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10TeV.mac
 
-git 
+git add -A
+git commit -m "Auto update $(date '+%F %T')" || true
+git push
