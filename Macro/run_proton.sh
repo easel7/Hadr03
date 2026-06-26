70b3854c1e106d14afc13a0bb39c1fcd137411f8
easel7 <blankeasel7@gmail.com>
2026-06-26 15:55:44 +0200

    update

---
+++ Macro/run_proton.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+exe=/Users/xiongzheng/software/Hadr03/build/Hadr03
+Tag=Proton
+
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_100GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_1TeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10TeV.mac
