c48348fd0494190af5c27d85862b24c170631253
easel7 <blankeasel7@gmail.com>
2026-06-26 16:27:38 +0200

    Auto update 2026-06-26 16:27:38

--- Macro/run_proton.sh
+++ Macro/run_proton.sh
@@ -6,3 +6,5 @@ ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10GeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_100GeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_1TeV.mac
 ${exe} /Users/xiongzheng/software/Hadr03/Macro/${Tag}_10TeV.mac
+
+git 
