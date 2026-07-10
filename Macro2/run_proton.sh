81e488d3b1ee3a18166596c9b84e27d82f5b57d3
easel7 <blankeasel7@gmail.com>
2026-07-10 14:38:11 +0200

    Auto update 2026-07-10 14:38:11

--- Macro2/run_proton.sh
+++ Macro2/run_proton.sh
@@ -66,8 +66,17 @@ Tag=Proton
 # ${exe} /Users/xiongzheng/software/Hadr03/Macro2/He4_8000GeV.mac
 # ${exe} /Users/xiongzheng/software/Hadr03/Macro2/He4_10000GeV.mac
 
-# ${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_100000GeV.mac
-${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_500000GeV.mac
+
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_12000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_16000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_20000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_25000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_32000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_49000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_50000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_63000GeV.mac
+${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_80000GeV.mac
+# ${exe} /Users/xiongzheng/software/Hadr03/Macro2/Proton_10000GeV.mac
 
 git add -A
 git commit -m "Auto update $(date '+%F %T')" || true
